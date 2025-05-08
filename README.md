c1ae899270ecd391afafed4984e83216f5280eb0
fly <filippo.panighello@studenti.unipd.it>
2025-05-08 10:56:04 +0200

    close #4 implementazione metodo main

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<Github action badge> <Coveralls badge>  
+`<Github action badge>` [![Coverage Status](https://coveralls.io/repos/github/Fly-PD/MTSS-test3/badge.svg?branch=main)](https://coveralls.io/github/Fly-PD/MTSS-test3?branch=main)  
 # Prova pratica 2 MTSS  
 Filippo Panighello - 2111016  
 Riccardo Manisi - 9111948
--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -7,6 +7,26 @@ package it.unipd.mtss;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int number = parseArgs(args);                   // Parsing dell'input
+
+        String output = RomanPrinter.print( number );   // Conversione in asciiArt numero romano
+        System.out.println( output );
+    }
+
+    // Parsing degli argomenti della riga di comando
+    // Input deve essere un'intero compreso fra 1 e 1000
+    private static int parseArgs(String[] args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Usage: java -jar roman-number.jar <number>");
+        }
+        try {
+            int number = Integer.parseInt(args[0]);
+            if (number < 1 || number > 1000) {
+                throw new IllegalArgumentException("Il numero deve essere compreso fra 1 e 1000");
+            }
+            return number;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Il numero deve essere un intero compreso fra 1 e 1000");
+        }
     }
 }
