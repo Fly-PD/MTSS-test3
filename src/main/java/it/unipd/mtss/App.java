b0b0f7104d9937b56d93a09fd8477c99758aa07c
fly <filippo.panighello@studenti.unipd.it>
2025-05-08 10:31:27 +0200

    close #1 configurazione analisi statica del codice

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -1,8 +1,10 @@
+////////////////////////////////////////////////////////////////////
+// [Filippo]  [Panighello] [2111016]
+// [Riccardo] [Manisi]     [9111948]
+////////////////////////////////////////////////////////////////////
+
 package it.unipd.mtss;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
